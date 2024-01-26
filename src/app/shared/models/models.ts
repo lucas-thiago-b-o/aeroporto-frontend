b6c8a74b01bc0cd7fa7cbc8751e9ee91cfab42c5
Lucas Thiago <lucas-thiago-b-o@proton.me>
2024-01-26 18:51:11 -0300

    models dos components e requisições

---
+++ src/app/shared/models/models.ts
@@ -0,0 +1,79 @@
+export interface AeroportoDTO {
+    id: number;
+    nome: string;
+    codigoAeroportuario: string;
+    cidade: CidadeDTO;
+}
+
+export interface AssentoDTO {
+    id: number;
+    nome: string;
+}
+
+export interface BagagemDTO {
+    id: number;
+    numeroIdentificacao: string;
+    isDespachada: boolean;
+}
+
+export interface CidadeDTO {
+    id: number;
+    nome: string;
+    uf: UfDTO;
+}
+
+export interface ClasseDTO {
+    id: number;
+    nome: string;
+    assentos: AssentoDTO[];
+}
+
+export interface ComprarPassagemDTO {
+    passagemDTO: PassagemDTO;
+    passageirosAssentoDTO: PassageiroAssentoDTO[];
+}
+
+export interface PassageiroAssentoDTO {
+    passageiroDTO: PassageiroDTO;
+    classeId: number;
+}
+
+export interface PassageiroDTO {
+    id: number;
+    nomeCompleto: string;
+    cpf: number;
+    rg: number;
+    passaporte: number;
+    dataNascimentodataNascimento: Date;
+    telefone: number;
+    contatoEmergencia: number;
+    bagagens: BagagemDTO[];
+}
+
+export interface PassagemDTO {
+    id: number;
+    portaoEmbarque: string;
+    uuidUsuario: string;
+    dataHoraVoo: Date;
+    valor: number;
+    numeroIdentificacao: string;
+    voo: VooDTO;
+}
+
+export interface UfDTO {
+    id: number;
+    nome: string;
+}
+
+export interface VooDTO {
+    id: number;
+    nome: string;
+    dataHoraMarcado: Date;
+    dataHoraPartida: Date;
+    dataHoraPrevisao: Date;
+    dataHoraChegada: Date;
+    aeroportoOrigem: AeroportoDTO;
+    aeroportoDestino: AeroportoDTO;
+    passageiros: PassageiroDTO[];
+    classes: ClasseDTO[];
+}
