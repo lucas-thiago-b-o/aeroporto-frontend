8e9e4e46dc14532a7a66b0a8fec9e0a3f20ce59a
Lucas Thiago <lucas-thiago-b-o@proton.me>
2024-02-04 06:07:27 -0300

    atualizações no componente de preenchimento de dados

--- src/app/components/views/preenchimento-dados/preenchimento-dados.component.ts
+++ src/app/components/views/preenchimento-dados/preenchimento-dados.component.ts
@@ -22,6 +22,67 @@ export class PreenchimentoDadosComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  checaTelefoneLength(value: string) {
+    const telefone = this.dadosFormGroup.get('telefone');
+
+    if (value.replace(/\D/g, '').length >= 10 && value.replace(/\D/g, '').length <= 11) {
+      telefone.setErrors(null);
+    } else {
+      telefone.setErrors({ incorrect: true });
+    }
+
+    telefone.markAsTouched();
+  }
+
+  checaContatoEmergenciaLength(value: string) {
+    const contatoEmergencia = this.dadosFormGroup.get('contatoEmergencia');
+
+    if (value.replace(/\D/g, '').length >= 10 && value.replace(/\D/g, '').length <= 11) {
+      contatoEmergencia.setErrors(null);
+    } else {
+      contatoEmergencia.setErrors({ incorrect: true });
+    }
+
+    contatoEmergencia.markAsTouched();
+  }
+
+  checaDataLength(value: string) {
+    const dataNascimento = this.dadosFormGroup.get('dataNascimento');
+
+    if (value.replace(/\D/g, '').length === 8) {
+      dataNascimento.setErrors(null);
+    } else {
+      dataNascimento.setErrors({ incorrect: true });
+    }
+
+    dataNascimento.markAsTouched();
+  }
+
+  checaRGLength(value: string) {
+    const rg = this.dadosFormGroup.get('rg');
+
+    if (value.replace(/\D/g, '').length === 9) {
+      rg.setErrors(null);
+    } else {
+      rg.setErrors({ incorrect: true });
+    }
+
+    rg.markAsTouched();
+  }
+
+
+  checaCPFLength(value: string) {
+    const cpf = this.dadosFormGroup.get('cpf');
+
+    if (value.replace(/\D/g, '').length === 11) {
+      cpf.setErrors(null);
+    } else {
+      cpf.setErrors({ incorrect: true });
+    }
+
+    cpf.markAsTouched();
+  }
+
   emiteValores() {
     this.preencheDados.emit(this.dadosFormGroup);
   }
